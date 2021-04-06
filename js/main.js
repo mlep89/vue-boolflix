f776f7a7c67ebfdb820e7f0b173f887a32417cde
Marco <mr.lepore@hotmail.it>
2021-04-06 14:28:13 +0200

    stampa corretta dei dati richiesti dall'esercizio (devo collegare l'input alle api)

--- js/main.js
+++ js/main.js
@@ -2,13 +2,15 @@ var root = new Vue({
     el: '#root',
     data: {
     films: [],
+    userSearch: "",
     },
     mounted() { 
+        const self = this;
         axios
         .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query=yuppies')
         .then(function (response) {
-            films = response.data.results;
-            console.log(films);
+            self.films = response.data.results;
+            console.log(self.films);
         })
     }
     });
