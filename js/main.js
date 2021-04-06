154cc9b036e2a0fdea8aaf11e74d8f945106cf39
Marco <mr.lepore@hotmail.it>
2021-04-06 12:34:11 +0200

    effettuata chiamata axios + istanziazione ciclo v-for in html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var root = new Vue({
     el: '#root',
     data: {
-    message: 'Hello World'
+    films: [],
     },
+    mounted() { 
+        axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query=yuppies')
+        .then(function (response) {
+            films = response.data.results;
+            console.log(films);
+        })
+    }
     });
