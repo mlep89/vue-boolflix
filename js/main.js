a054a918136c67b121cd842692d893358e689c10
Marco <mr.lepore@hotmail.it>
2021-04-07 11:54:37 +0200

    aggiunta milestone 3 (visualizzazione locandina film)

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ var root = new Vue({
     el: '#root',
     data: {
     films: [],
-    userSearch: "",
+    userSearch: 'yuppies',
     },
-    mounted() { 
-        const self = this;
+    methods: {
+        searchFilm: function(){
         axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query=yuppies')
-        .then(function (response) {
-            self.films = response.data.results;
-            console.log(self.films);
+        .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
+        .then( (response) => {
+            this.films = response.data.results;
+            
         })
+        }
+    },
+    mounted() {
+        this.searchFilm();
     }
     });
