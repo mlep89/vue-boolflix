dde28de5e3be3c04bd20e9a7c0dd4ca42c688573
Marco <mr.lepore@hotmail.it>
2021-04-07 16:03:24 +0200

    aggiunta ricerca  di serie tv unitamente ai film

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var root = new Vue({
     el: '#root',
     data: {
     films: [],
+    series: [],
     userSearch: 'yuppies',
     },
     methods: {
@@ -10,8 +11,14 @@ var root = new Vue({
         .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
         .then( (response) => {
             this.films = response.data.results;
-            
         })
+        },
+        searchSerie: function(){
+            axios
+            .get('https://api.themoviedb.org/3/search/tv?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&query='+ this.userSearch)
+            .then( (response) => {
+                this.series = response.data.results;
+            })
         }
     },
     mounted() {
