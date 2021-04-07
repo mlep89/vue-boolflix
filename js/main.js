d48c74dab5b26b0a3fee3c4b04e01b8c4705bbbe
Marco <mr.lepore@hotmail.it>
2021-04-07 17:19:42 +0200

    riduzione valutazione film/serie tv a numeri interi con max valore 5

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ var root = new Vue({
                 axios
                 .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
                 .then( (response) => {
+                    response.data.results.forEach(film => {
+                        film.vote_average = Math.ceil(film.vote_average)/2;
+                    })
                     this.films = response.data.results;
                 })
             }
@@ -23,11 +26,19 @@ var root = new Vue({
                 axios
                 .get('https://api.themoviedb.org/3/search/tv?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
                 .then( (response) => {
+                    response.data.results.forEach(serie => {
+                        serie.vote_average = Math.ceil(serie.vote_average)/2;
+                    })
                     this.series = response.data.results;
                 })
             }
             else {
                 this.series = [];
+            }
+        },
+        starValue: function(){
+            for (i=0; i < 6; i++) {
+                
             }
         }
     }
