110fe64233e7bc3aa790b5cea39b859c5a639cd6
Marco <mr.lepore@hotmail.it>
2021-04-07 16:40:20 +0200

    sistemazione ricerca iniziale con valori vuoti

--- js/main.js
+++ js/main.js
@@ -3,25 +3,35 @@ var root = new Vue({
     data: {
     films: [],
     series: [],
-    userSearch: 'yuppies',
+    userSearch: '',
     },
     methods: {
-        searchFilm: function(){
-        axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
-        .then( (response) => {
-            this.films = response.data.results;
-        })
+        searchFilm: function(userSearch){
+            if (userSearch != '') {
+                axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
+                .then( (response) => {
+                    this.films = response.data.results;
+                })
+            }
+            else {
+                this.films = [];
+            }
         },
-        searchSerie: function(){
-            axios
-            .get('https://api.themoviedb.org/3/search/tv?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&query='+ this.userSearch)
-            .then( (response) => {
-                this.series = response.data.results;
-            })
+        searchSerie: function(userSearch){
+            if (userSearch != '') {
+                axios
+                .get('https://api.themoviedb.org/3/search/tv?api_key=e1e83484b58b02a9d475b9cb41829bfe&language=it-IT&page=1&include_adult=false&query='+ this.userSearch)
+                .then( (response) => {
+                    this.series = response.data.results;
+                })
+            }
+            else {
+                this.series = [];
+            }
         }
-    },
-    mounted() {
-        this.searchFilm();
     }
     });
+
+
+    
